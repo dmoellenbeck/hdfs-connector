d613f6cc57d9d55c8815fbfdf3015c5c1f168d69
Mariano Quintela <mquintela@mulesoft.com>
2015-03-30 20:39:17 +0000

    Connector Documentation Deploy

---
+++ 4.0.0/demo/src/main/java/org/mulesoft/demo/RegexIncludePathFilter.java
@@ -0,0 +1,23 @@
+/**
+ * (c) 2003-2015 MuleSoft, Inc. The software in this package is
+ * published under the terms of the CPAL v1.0 license, a copy of which
+ * has been included with this distribution in the LICENSE.md file.
+ */
+
+package org.mulesoft.demo;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.PathFilter;
+
+public class RegexIncludePathFilter implements PathFilter{
+
+private final String regex;
+	
+	public RegexIncludePathFilter(String regex){
+		this.regex = regex;
+	}
+	@Override
+	public boolean accept(Path path) {
+		return path.toString().matches(regex);
+	}
+}
