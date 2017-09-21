64f690897462ffdfa25cd5b262d337543b67faa3
cosmin.campian <cosmin.campian@mulesoft.com>
2017-09-21 14:59:13 +0300

    -add tests to suite

--- src/test/java/org/mule/extension/hdfs/automation/runner/FunctionalTestSuite.java
+++ src/test/java/org/mule/extension/hdfs/automation/runner/FunctionalTestSuite.java
@@ -6,14 +6,37 @@ package org.mule.extension.hdfs.automation.runner;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+import org.mule.extension.hdfs.automation.functional.AppendTestCases;
+import org.mule.extension.hdfs.automation.functional.CopyFromLocalFileTestCases;
+import org.mule.extension.hdfs.automation.functional.CopyToLocalFileTestCases;
+import org.mule.extension.hdfs.automation.functional.DeleteDirectoryTestCases;
+import org.mule.extension.hdfs.automation.functional.DeleteFileTestCases;
+import org.mule.extension.hdfs.automation.functional.GetMetadataTestCases;
+import org.mule.extension.hdfs.automation.functional.GlobStatusTestCases;
+import org.mule.extension.hdfs.automation.functional.ListStatusTestCases;
+import org.mule.extension.hdfs.automation.functional.MakeDirectoriesTestCases;
 import org.mule.extension.hdfs.automation.functional.ReadOperationTestCases;
+import org.mule.extension.hdfs.automation.functional.RenameTestCases;
+import org.mule.extension.hdfs.automation.functional.SetOwnerTestCases;
+import org.mule.extension.hdfs.automation.functional.SetPermissionTestCases;
 import org.mule.extension.hdfs.automation.functional.WriteTestCases;
 
 @RunWith(Suite.class)
 @SuiteClasses({
+        AppendTestCases.class,
+        CopyFromLocalFileTestCases.class,
+        CopyToLocalFileTestCases.class,
+        DeleteDirectoryTestCases.class,
+        DeleteFileTestCases.class,
+        GetMetadataTestCases.class,
+        GlobStatusTestCases.class,
+        ListStatusTestCases.class,
+        MakeDirectoriesTestCases.class,
         ReadOperationTestCases.class,
-        WriteTestCases.class,
-
+        RenameTestCases.class,
+        SetOwnerTestCases.class,
+        SetPermissionTestCases.class,
+        WriteTestCases.class
 })
 public class FunctionalTestSuite {
 
