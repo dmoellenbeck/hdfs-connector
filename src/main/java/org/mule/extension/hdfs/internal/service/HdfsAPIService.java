aa8cce0f1f03bcd20c2f88d434258b572e1fe1cb
danpece <dan.pece@mulesoft.com>
2017-11-15 14:30:26 +0200

    Hdfsc 50 exception handler (#63)
    * HDFSC-50 fix review issues
    
    * HDFSC-50 fix review issues
    
    * HDFSC-50 fix review issues - errorhandling
    
    * HDFSC-50 fix review issues - errorhandling
    
    * HDFSC-50 fix review issues - removed unused ex

--- src/main/java/org/mule/extension/hdfs/internal/service/HdfsAPIService.java
+++ src/main/java/org/mule/extension/hdfs/internal/service/HdfsAPIService.java
@@ -10,44 +10,44 @@ import org.mule.extension.hdfs.api.FileStatus;
 import org.mule.extension.hdfs.api.MetaData;
 import org.mule.extension.hdfs.internal.service.exception.HdfsConnectionException;
 import org.mule.extension.hdfs.internal.service.exception.InvalidRequestDataException;
-import org.mule.extension.hdfs.internal.service.exception.UnableToRetrieveResponseException;
-import org.mule.extension.hdfs.internal.service.exception.UnableToSendRequestException;
+
+
 
 public interface HdfsAPIService {
 
-    InputStream read(String path, int bufferSize) throws InvalidRequestDataException, UnableToRetrieveResponseException, UnableToSendRequestException, HdfsConnectionException;
+    InputStream read(String path, int bufferSize) throws InvalidRequestDataException, HdfsConnectionException;
 
     void create(String path, String permission, boolean overwrite, int bufferSize, int replication, long blockSize, String ownerUserName, String ownerGroupName,
-            InputStream payload) throws InvalidRequestDataException, UnableToRetrieveResponseException, UnableToSendRequestException, HdfsConnectionException;
+            InputStream payload) throws InvalidRequestDataException, HdfsConnectionException;
 
     List<FileStatus> listStatus(String path, String filter)
-            throws InvalidRequestDataException, UnableToRetrieveResponseException, UnableToSendRequestException, HdfsConnectionException;
+            throws InvalidRequestDataException, HdfsConnectionException;
 
     List<FileStatus> globStatus(String pathPattern, String filter)
-            throws InvalidRequestDataException, UnableToRetrieveResponseException, UnableToSendRequestException, HdfsConnectionException;
+            throws InvalidRequestDataException, HdfsConnectionException;
 
-    void mkdirs(String path, String permission) throws InvalidRequestDataException, UnableToRetrieveResponseException, UnableToSendRequestException, HdfsConnectionException;
+    void mkdirs(String path, String permission) throws InvalidRequestDataException, HdfsConnectionException;
 
-    void deleteDirectory(String path) throws InvalidRequestDataException, UnableToRetrieveResponseException, UnableToSendRequestException, HdfsConnectionException;
+    void deleteDirectory(String path) throws InvalidRequestDataException, HdfsConnectionException;
 
     void append(String path, int bufferSize, InputStream payload)
-            throws InvalidRequestDataException, UnableToRetrieveResponseException, UnableToSendRequestException, HdfsConnectionException;;
+            throws InvalidRequestDataException, HdfsConnectionException;;
 
-    void deleteFile(String path) throws InvalidRequestDataException, UnableToRetrieveResponseException, UnableToSendRequestException, HdfsConnectionException;
+    void deleteFile(String path) throws InvalidRequestDataException, HdfsConnectionException;
 
-    MetaData getMetadata(String path) throws InvalidRequestDataException, UnableToRetrieveResponseException, UnableToSendRequestException, HdfsConnectionException;
+    MetaData getMetadata(String path) throws InvalidRequestDataException, HdfsConnectionException;
 
-    void rename(String source, String destination) throws InvalidRequestDataException, UnableToRetrieveResponseException, UnableToSendRequestException, HdfsConnectionException;
+    void rename(String source, String destination) throws InvalidRequestDataException, HdfsConnectionException;
 
     void copyToLocalFile(boolean deleteSource, boolean useRawLocalFileSystem, String source, String destination)
-            throws InvalidRequestDataException, UnableToRetrieveResponseException, UnableToSendRequestException, HdfsConnectionException;
+            throws InvalidRequestDataException, HdfsConnectionException;
 
     void copyFromLocalFile(boolean deleteSource, boolean overwrite, String source, String destination)
-            throws InvalidRequestDataException, UnableToRetrieveResponseException, UnableToSendRequestException, HdfsConnectionException;
+            throws InvalidRequestDataException, HdfsConnectionException;
 
-    void setPermission(String path, String permission) throws InvalidRequestDataException, UnableToRetrieveResponseException, UnableToSendRequestException, HdfsConnectionException;
+    void setPermission(String path, String permission) throws InvalidRequestDataException, HdfsConnectionException;
 
     void setOwner(String path, String ownername, String groupname)
-            throws InvalidRequestDataException, UnableToRetrieveResponseException, UnableToSendRequestException, HdfsConnectionException;
+            throws InvalidRequestDataException, HdfsConnectionException;
 
 }
